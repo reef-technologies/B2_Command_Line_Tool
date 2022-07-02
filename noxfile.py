92b9d703cef73ec8ff4ab2fbfa743bce1a44ae5c
Pawel Polewicz <p.polewicz@gmail.com>
2022-07-02 14:24:51 +0200

    Temporarily disable xdist due to racing on sqlite dbs

--- noxfile.py
+++ noxfile.py
@@ -156,9 +156,7 @@ def integration(session):
     """Run integration tests."""
     install_myself(session)
     session.install(*REQUIREMENTS_TEST)
-    session.run(
-        'pytest', '-s', '-n', str(min(cpu_count(), 8) * 5), *session.posargs, 'test/integration'
-    )
+    session.run('pytest', '-s', *session.posargs, 'test/integration')
 
 
 @nox.session(python=PYTHON_VERSIONS)
