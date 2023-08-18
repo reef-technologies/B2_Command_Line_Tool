3cad7c0eb3574eea0dea383220f76824f60653fa
Maciej Urbanski <maciej.rooter.urbanski@reef.pl>
2023-08-18 09:56:28 +0200

    add short integration tests description

--- test/integration/__init__.py
+++ test/integration/__init__.py
@@ -7,3 +7,10 @@
 # License https://www.backblaze.com/using_b2_code.html
 #
 ######################################################################
+"""
+Integration tests for the `b2` CLI.
+
+Integration tests:
+ * MUST run against `b2` binary (see `--sut` option in conftest.py)
+ * MAY use real B2 account and be run against production servers
+"""
