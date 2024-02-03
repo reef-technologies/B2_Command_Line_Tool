6479ad1b923dd385bff37b5d1c193a5fad5c5256
Michal Przadka <przadka@gmail.com>
2024-02-03 11:59:30 +0100

    Audit script + notes

---
+++ INCONSISTENT.md
@@ -0,0 +1,28 @@
+Inconsistenticies to fix:
+
+1. file-info with b2:/. does not contain `contentMd5` and with b2id:// does
+
+2. if key does not have read access `isFileLockEnabled` property should be set to `unknown` (currently: `"isFileLockEnabled": null`)
+
+3. what does `defaultRetention` `null` mean? does it mean that it is none?:
+
+    "defaultRetention": {
+        "mode": null
+    },
+
+check this
+
+
+Even: introduce a possible value of "mode": "unknown" and for buckets with no default encryption use "mode": null.
+
+
+4. delete-bucket, copy-file, succesed silently
+deleting file version with delete-file-version ouputs json
+
+5. creating new bucket does not return json, just the bucket id
+
+6. list buckets does not return json but something else
+
+check other commands
+
+create-key. delete-key, list-buckets
--- audit_cli.sh
+++ audit_cli.sh
@@ -10,48 +10,96 @@ LOCAL_FILE_PATH="/home/michal/Downloads/83829173_58.pdf"
 B2_FILE_NAME="b2_test_file.pdf" # Name for the file in B2
 COPY_FILE_NAME="copied_${B2_FILE_NAME}"
 DOWNLOAD_PATH="downloaded_${B2_FILE_NAME}"
+FOLDER_NAME="sample_folder"
 
 # List buckets
-echo "Listing all buckets"
+echo
+echo ">>>>>>> Listing all buckets"
 ./dist/b2 list-buckets
 
 # Upload a file
-echo "Uploading a file to B2"
+echo
+echo ">>>>>>> Uploading a file to B2"
 ./dist/b2 upload-file $BUCKET_NAME $LOCAL_FILE_PATH $B2_FILE_NAME
 
 # List files in a bucket
-echo "Listing files in the bucket"
+echo
+echo ">>>>>>> Listing files in the bucket"
 ./dist/b2 ls $BUCKET_NAME
 
 # Copy a file by ID
-echo "Copying file by ID"
-# Copy a file by ID and parse the output to get the new file ID
+echo
+echo ">>>>>>> Copying file by ID"
 COPIED_FILE_JSON=$(./dist/b2 copy-file-by-id $FILE_ID $BUCKET_NAME $COPY_FILE_NAME)
 COPIED_FILE_ID=$(echo "$COPIED_FILE_JSON" | jq -r '.fileId')
 
 # Delete the copied file version
-echo "Deleting the copied file version"
-# Retrieve the file ID of the copied file first, then delete. This part needs manual handling or scripting to parse output.
+echo
+echo ">>>>>>> Deleting the copied file version"
 ./dist/b2 delete-file-version $COPY_FILE_NAME $COPIED_FILE_ID
 
-
-
 # Download a file by id
-echo "Downloading a file with download-file-by-id"
+echo
+echo ">>>>>>> Downloading a file with download-file-by-id"
 ./dist/b2 download-file-by-id $FILE_ID $DOWNLOAD_PATH
 
 # Download a file
-echo "Downloading a file with download-file"
+echo
+echo ">>>>>>> Downloading a file with download-file"
 ./dist/b2 download-file b2id://$FILE_ID $DOWNLOAD_PATH
 
 # Get file info
-echo "Getting file information using file ID"
+echo
+echo ">>>>>>> Getting file information using file ID"
 ./dist/b2 file-info b2id://$FILE_ID
 
+# get file info by name
+echo
+echo ">>>>>>> Getting file information using file name"
+./dist/b2 file-info b2://$BUCKET_NAME/$B2_FILE_NAME 
 
+# Remove a file or a directory
+echo
+echo ">>>>>>> Removing a file/directory"
+./dist/b2 rm --dryRun --queueSize 5 --noProgress --failFast --threads 2 --versions -r --withWildcard $BUCKET_NAME $FOLDER_NAME
 
+# Get file URL
+echo
+echo ">>>>>>> Getting file URL"
+./dist/b2 get-url b2id://$FILE_ID
+
+# Update bucket settings
+echo
+echo ">>>>>>> Updating bucket settings"
+./dist/b2 update-bucket --fileLockEnabled --defaultServerSideEncryption "SSE-B2" --defaultServerSideEncryptionAlgorithm "AES256" $BUCKET_NAME allPrivate
+
+# Hide a file in a bucket
+echo
+echo ">>>>>>> Hiding a file in the bucket"
+./dist/b2 hide-file $BUCKET_NAME $B2_FILE_NAME
+
+# Create a new bucket
+echo
+echo ">>>>>>> Creating a new bucket"
+./dist/b2 create-bucket "$BUCKET_NAME-new" allPublic
+
+# Delete a bucket
+echo
+echo ">>>>>>> Deleting a bucket"
+./dist/b2 delete-bucket "$BUCKET_NAME-new"
+
+# Get account info
+echo
+echo ">>>>>>> Getting account information"
+./dist/b2 get-account-info
+
+# List files with options
+echo
+echo ">>>>>>> Listing files with options"
+./dist/b2 ls --long --json --replication --versions -r $BUCKET_NAME
 
 # Clean up: delete downloaded file
 rm -f $DOWNLOAD_PATH
 
-echo "Done with B2 CLI operations."
+echo
+echo ">>>>>>> Done with B2 CLI operations."
