3ae39949b6bf70d8a327cf39eb875762133d1847
Michal Przadka <przadka@gmail.com>
2024-02-04 17:13:48 +0100

    Audit script improved

--- audit_cli.sh
+++ audit_cli.sh
@@ -19,7 +19,7 @@ source .env
 
 # Variables
 BUCKET_NAME="mici-bucket"
-FILE_ID="4_z70b22389f0bf88e4877f0e12_f112c45fd5ce1bfce_d20240131_m154715_c005_v0501004_t0059_u01706716035791"
+FILE_ID=""
 LOCAL_FILE_PATH="/home/michal/Downloads/83829173_58.pdf"
 B2_FILE_NAME="b2_test_file.pdf" # Name for the file in B2
 COPY_FILE_NAME="copied_${B2_FILE_NAME}"
@@ -27,7 +27,6 @@ DOWNLOAD_PATH="downloaded_${B2_FILE_NAME}"
 FOLDER_NAME="sample_folder"
 EMPTY_FOLDER="/home/michal/dev/B2/empty"
 KEY_NAME="myKey"
-KEY_TO_DELETE="keyIdToDelete"
 
 LOCAL_DIR_PATH="/home/michal/Downloads"  # Local directory to sync
 B2_DIR_PATH="b2://$BUCKET_NAME/"  # B2 bucket path where the local directory will be synced
@@ -45,6 +44,12 @@ read -r KEY_ID APPLICATION_KEY <<< "$KEY_OUTPUT"
 echo "Key ID: $KEY_ID"
 echo "Application Key: $APPLICATION_KEY"
 
+# List all keys
+echo
+echo ">>>>>>> Listing all application keys"
+echo "./dist/b2 list-keys"
+./dist/b2 list-keys
+
 # List buckets
 echo
 echo ">>>>>>> Listing all buckets"
@@ -57,11 +62,30 @@ echo ">>>>>>> Uploading a file to B2"
 echo "./dist/b2 upload-file --noProgress $BUCKET_NAME $LOCAL_FILE_PATH $B2_FILE_NAME"
 ./dist/b2 upload-file --noProgress $BUCKET_NAME $LOCAL_FILE_PATH $B2_FILE_NAME
 
+# List all versions of a file
+echo
+echo ">>>>>>> Listing all versions of a file"
+echo "./dist/b2 ls --versions $BUCKET_NAME $B2_FILE_NAME"
+./dist/b2 ls --versions $BUCKET_NAME $B2_FILE_NAME
+
 # List files in a bucket
 echo
 echo ">>>>>>> Listing files in the bucket"
 echo "./dist/b2 ls $BUCKET_NAME"
 ./dist/b2 ls $BUCKET_NAME
+
+# Check replication status
+#echo
+#echo ">>>>>>> Checking replication status"
+#echo "./dist/b2 replication-status $BUCKET_NAME"
+#./dist/b2 replication-status $BUCKET_NAME
+
+
+# Get a random file ID
+echo
+echo ">>>>>>> Getting a random file ID"
+echo "./dist/b2 ls --json $BUCKET_NAME"
+FILE_ID=$(./dist/b2 ls --json $BUCKET_NAME | jq -r '.[0].fileId')
 
 # Copy a file by ID
 echo
@@ -113,6 +137,25 @@ echo ">>>>>>> Getting file URL"
 echo "./dist/b2 get-url b2id://$FILE_ID"
 ./dist/b2 get-url b2id://$FILE_ID
 
+# Update file legal hold, to on
+echo
+echo ">>>>>>> Updating file legal hold"
+echo "./dist/b2 update-file-legal-hold $B2_FILE_NAME $FILE_ID on"
+./dist/b2 update-file-legal-hold $B2_FILE_NAME $FILE_ID on
+
+# Update file legal hold, to off
+echo
+echo ">>>>>>> Updating file legal hold"
+echo "./dist/b2 update-file-legal-hold $B2_FILE_NAME $FILE_ID off"
+./dist/b2 update-file-legal-hold $B2_FILE_NAME $FILE_ID off
+
+# Update file retention settings
+echo
+echo ">>>>>>> Updating file retention settings"
+echo "./dist/b2 update-file-retention --retainUntil \"1807062189\" $FILE_ID compliance"
+./dist/b2 update-file-retention --retainUntil "1807062189" $FILE_ID compliance
+
+
 # Update bucket settings
 echo
 echo ">>>>>>> Updating bucket settings"
@@ -131,6 +174,12 @@ echo ">>>>>>> Creating a new bucket"
 echo "./dist/b2 create-bucket \"$BUCKET_NAME-new\" allPublic"
 ./dist/b2 create-bucket "$BUCKET_NAME-new" allPublic
 
+# Set the replication setup
+echo
+echo ">>>>>>> Setting the replication setup"
+echo "./dist/b2 replication-setup $BUCKET_NAME $BUCKET_NAME-new"
+./dist/b2 replication-setup $BUCKET_NAME $BUCKET_NAME-new
+
 # Delete a bucket
 echo
 echo ">>>>>>> Deleting a bucket"
@@ -140,8 +189,8 @@ echo "./dist/b2 delete-bucket \"$BUCKET_NAME-new\""
 # Delete an application key
 echo
 echo ">>>>>>> Deleting an application key"
-echo "./dist/b2 delete-key $KEY_TO_DELETE"
-./dist/b2 delete-key $KEY_TO_DELETE
+echo "./dist/b2 delete-key $KEY_ID"
+./dist/b2 delete-key $KEY_ID
 
 # Get account info
 echo
@@ -181,35 +230,9 @@ echo "./dist/b2 sync --noProgress --allowEmptySource --delete $EMPTY_FOLDER $B2_
 echo
 echo ">>>>>>> Done with B2 CLI operations."
 
-## TODO: Add more operations to test the B2 CLI
-# echo "Listing all application keys:"
-# ./dist/b2 list-keys
-
-# echo "Listing all versions of a file:"
-# ./dist/b2 ls --versions "$BUCKET_NAME" "$B2_FILE_NAME"
-
-
-# echo "Starting a large file upload (simulated):"
-# You would need to simulate or actually start a large file upload here
-
-# echo "Listing parts of the large file upload:"
-#./dist/b2 list-parts "$LARGE_FILE_ID"
-
-#echo "Canceling the large file upload:"
-#./dist/b2 cancel-large-file "$LARGE_FILE_ID"
-
-#echo "Setting up replication:"
-# Use replication-setup with appropriate parameters here
 
 #echo "Checking replication status:"
-./dist/b2 replication-status "$SOURCE_BUCKET_NAME"
+#./dist/b2 replication-status "$SOURCE_BUCKET_NAME"
 
-#echo "Updating bucket with lifecycle rules:"
-# This requires a JSON string for lifecycle rules
-#./dist/b2 update-bucket --lifecycleRules '[{"fileNamePrefix":"","daysFromUploadingToHiding":30,"daysFromHidingToDeleting":365}]' "$BUCKET_NAME" allPrivate
 
-#echo "Updating file legal hold:"
-./dist/b2 update-file-legal-hold "$B2_FILE_NAME" "$FILE_ID" on
 
-#echo "Updating file retention settings:"
-#./dist/b2 update-file-retention --retainUntil "2024-01-01" "$B2_FILE_NAME" "$FILE_ID" compliance
